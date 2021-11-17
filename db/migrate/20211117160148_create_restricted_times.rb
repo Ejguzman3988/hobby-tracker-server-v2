8ddfdc87b399ba862b81199ab5d93baaa4fea066
ejguzman3988 <eriberto.guzman@outlook.com>
2021-11-17 11:07:32 -0500

    Fixes minor spelling mistakes in migrations

--- db/migrate/20211117160148_create_restricted_times.rb
+++ db/migrate/20211117160148_create_restricted_times.rb
@@ -2,8 +2,7 @@ class CreateRestrictedTimes < ActiveRecord::Migration[6.1]
   def change
     create_table :restricted_times do |t|
       t.string :name
-      t.string :total_time
-      t.string :int
+      t.integer :total_time
       t.belongs_to :user, null: false, foreign_key: true
 
       t.timestamps
--- db/migrate/20211117160320_create_daily_stats.rb
+++ db/migrate/20211117160320_create_daily_stats.rb
@@ -1,10 +1,10 @@
 class CreateDailyStats < ActiveRecord::Migration[6.1]
   def change
     create_table :daily_stats do |t|
-      t.integer :total_restrcited_time
+      t.integer :total_restricted_time
       t.integer :non_alloted_time
-      t.total :tracked_time
-      t.int :max_time
+      t.integer :tracked_time
+      t.integer :max_time
       t.belongs_to :user, null: false, foreign_key: true
 
       t.timestamps
