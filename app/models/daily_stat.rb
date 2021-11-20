a345b7d1b667082e7798f47526ebd5517df1ec16
ejguzman3988 <eriberto.guzman@outlook.com>
2021-11-20 08:46:16 -0500

    Adding restricted times configures nonAllotedTime

--- app/models/daily_stat.rb
+++ app/models/daily_stat.rb
@@ -1,5 +1,12 @@
 class DailyStat < ApplicationRecord
-  belongs_to :user, optional: true;
+  belongs_to :user, optional: true
   has_one :day
   has_many :timers, through: :days
+  validates :total_restricted_time, :non_alloted_time, :tracked_time, length: {maximum: 24, minimum: 0}
+
+
+  def meta_restricted_time=(time)
+    self.total_restricted_time = time
+    self.non_alloted_time -= time
+  end
 end
