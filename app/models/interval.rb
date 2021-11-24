0cf55df4f2b37bf67fd65fd7a03b41fa61f27787
ejguzman3988 <eriberto.guzman@outlook.com>
2021-11-24 09:38:03 -0500

    cofigures status to auto generate time started and totals

--- app/models/interval.rb
+++ app/models/interval.rb
@@ -1,4 +1,20 @@
 class Interval < ApplicationRecord
   belongs_to :daily_stat
   belongs_to :timer
+
+  def status=(activity)
+    super(activity)
+    case activity.downcase
+    when "start"
+      # This if statement deals with the pausing
+      if(self.time_started != nil)
+        self.time_started = Time.current
+      elsif (self.total_time)
+        self.time_started = Time.current - self.total_time
+      end
+    when "ended"
+      self.total_time = self.time_ended - self.time_start
+    end
+  end
+
 end
