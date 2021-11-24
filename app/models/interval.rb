fea773f906c64a54494b8e2782bb4dab422235ce
ejguzman3988 <eriberto.guzman@outlook.com>
2021-11-24 10:01:17 -0500

    adds the ability to change timers and daily stat total times when timer is paused

--- app/models/interval.rb
+++ app/models/interval.rb
@@ -1,20 +1,28 @@
 class Interval < ApplicationRecord
+
   belongs_to :daily_stat
   belongs_to :timer
 
   def status=(activity)
     super(activity)
     case activity.downcase
-    when "start"
+    when "play"
       # This if statement deals with the pausing
       if(self.time_started != nil)
         self.time_started = Time.current
       elsif (self.total_time)
         self.time_started = Time.current - self.total_time
       end
-    when "ended"
-      self.total_time = self.time_ended - self.time_start
+    when "pause"
+      self.time_ended = Time.current
+      self.total_time = self.time_ended - self.time_started
     end
+  end
+
+  def total_time=(tot)
+    super(tot)
+    self.daily_stat.tracked_time += tot
+    self.timer.total_time += tot
   end
 
 end
