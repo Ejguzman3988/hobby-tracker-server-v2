96e015d835ab669061ba7ff7caeec879d97c22e6
Eriebrto Guzman <eriberto.guzman@outlook.com>
2021-12-01 09:03:21 -0500

    Updates default stat when a restricted time is added

--- app/models/restricted_time.rb
+++ app/models/restricted_time.rb
@@ -1,3 +1,9 @@
 class RestrictedTime < ApplicationRecord
   belongs_to :user
+  def initialize(attr)
+    super(attr)
+
+    default_stat = User.find(self.user_id).default_stat
+    default_stat && default_stat.update(total_restricted_time: default_stat.total_restricted_time+self.total_time)
+  end
 end
